1aef28d64db67d952674fc0e3b7c54b4dada64c1
Goutham Guntnur (gguntnur) <goutham.g28@gmail.com>
2017-03-09 21:04:51 +0530

    changes for chat

--- src/main/java/com/cmad/service/MainVerticle.java
+++ src/main/java/com/cmad/service/MainVerticle.java
@@ -18,74 +18,14 @@ import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
 import io.vertx.ext.web.handler.StaticHandler;
 
+
 public class MainVerticle extends AbstractVerticle {
 	
 	private static Router router;
 
 	public void start(Future<Void> future) throws Exception {
 		startServer("start()");
-		/*
-
-
-		System.out.println("starting...");
-//		Vertx vertx = Vertx.vertx();
-		Router router = Router.router(vertx);
-		vertx.deployVerticle(LoginVerticle.class.getName(), new DeploymentOptions().setWorker(true));
-		vertx.deployVerticle(RegistrationVerticle.class.getName(), new DeploymentOptions().setWorker(true));
-	//------------------------------------------//	
-		router.route("/about").handler(rctx -> {
-			HttpServerResponse response = rctx.response();
-			response.putHeader("content-type", "text/html")
-					.end("<h1>Hello from my first Vert.x 3 application via routers</h1>");
-		});
-	//------------------------------------------//	
-		router.route("/*").handler(StaticHandler.create("spiderman"));
-		//router.route("/static/*").handler(StaticHandler.create("web"));
-	//------------------------------------------//	
-		router.route(Paths.P_REGISTRATION).handler(BodyHandler.create());
-		router.post(Paths.P_REGISTRATION).handler(rctx -> {
-			System.out.println("MainVerticle.start() inside register ");
-			String name = rctx.request().getParam("fullName");
-			String pwd= rctx.request().getParam("pwd");
-			String areaofinterest= rctx.request().getParam("areaofinterest");
-			System.out.println("MainVerticle.start() name "+name);
-			System.out.println("MainVerticle.start() pwd "+pwd);
-			System.out.println("MainVerticle.start() areaofinterest "+areaofinterest);
-			vertx.eventBus().send(Topics.REGISTRATION, rctx.getBodyAsJson(), r -> {
-				System.out.println("MainVerticle.start() register r "+r);
-				System.out.println("MainVerticle.start() register r.result() "+r.result());
-				if(r.result() != null)	{
-					System.out.println("MainVerticle.start() register r.result().body() "+r.result().body());
-					System.out.println("MainVerticle.start() register r.result().body().toString() "+r.result().body().toString());
-					rctx.response().setStatusCode(200).end(r.result().body().toString());
-				}	else	{
-					rctx.response().setStatusCode(404).end("No user created");
-				}
-			});
-			rctx.response().setStatusCode(200).putHeader("content-type", "application/json; charset=utf-8")
-					.end();
-		});
-	//------------------------------------------//	
-		router.route("/api/login").handler(BodyHandler.create());
-		router.post("/api/login").handler(rctx -> {
-			vertx.eventBus().send("com.cisco.cmad.projects.login", rctx.getBodyAsJson(), r -> {
-				System.out.println("MainVerticle.start() message "+r.result().body().toString());
-				rctx.response().setStatusCode(200).end(r.result().body().toString());
-//				rctx.response().setStatusCode(200).end(Json.encodePrettily(obj));
-			});
-		});
-		vertx.createHttpServer().requestHandler(router::accept).listen(8080);
-//		,
-//				result -> {
-//					if (result.succeeded()) {
-////						future.complete();
-//					} else {
-////						future.fail(result.cause());
-//					}
-//				});
-	//
-
-	*/}
+	}
 
 	private static void startServer(String str)	{
 		System.out.println("starting...from "+str);
@@ -125,7 +65,9 @@ public class MainVerticle extends AbstractVerticle {
 		setUpdateCommentsHandler(vertx);
 		// ------------------------------------------//
 
-		vertx.createHttpServer().requestHandler(router::accept).listen(8080);
+//		vertx.createHttpServer().requestHandler(router::accept).listen(8080);
+		vertx.createHttpServer().requestHandler(router::accept).websocketHandler(new CustomWebSocketHandler()).listen(8080);
+		
 		// ,
 		// result -> {
 		// if (result.succeeded()) {
@@ -393,4 +335,5 @@ System.out.println("MainVerticle.setBlogFetchHandler() entered");
 		
 		System.out.println("MainVerticle.printHTTPServerRequest() httpServerRequest.uri() = "+httpServerRequest.params());
 	}
+
 }
